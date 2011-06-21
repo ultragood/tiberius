1815cdfa4bf806adaca748c821579d5835cd8864
Mitchell Goodman <ultragood@gmail.com>
2011-06-21 15:52:05 -0400

    adding tests

---
+++ src/test/Test2.cpp
@@ -0,0 +1,6 @@
+
+#include "gtest/gtest.h"
+
+TEST(anotherTrivialTest, trivial2){
+    EXPECT_EQ(1, 2);
+}
---
+++ src/test/Trivial.cpp
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+
+TEST(trivialTest, trivial1){
+    EXPECT_EQ(1, 1);
+}
