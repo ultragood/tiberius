d6ad3082b5bc407fd023717fb5b0103676c47650
Michael Pettenato <michael.pettenato@gmail.com>
2011-06-24 14:51:32 -0400

    added more tests to the ridf function.  Might want to rename it isPhrase and have it call an ridf function since it has more pos logic than the ridf name would have you believe.  May be misleading

---
+++ src/Keywords/Phrase.h
@@ -0,0 +1,25 @@
+#ifndef PHRASE_H
+#define Phrase_H
+
+#include <vector>
+#include "fries/language.h"
+
+using namespace std;
+
+namespace Keywords {
+  class Phrase {
+  public:
+  Phrase(string phrase, double ridf) : phrase(phrase) { 
+      this->ridf= ridf;
+  }
+   
+    bool operator< (const Phrase &a) const {
+      return this->ridf < a.ridf;
+    }
+    //protected: 
+    double ridf;
+    string phrase;
+
+  };
+}
+#endif
